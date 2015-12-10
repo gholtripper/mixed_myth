3b377c857038abb10d98450c87a4e176042a95b1
Dick Davis <rdavis89@my.apsu.edu>
2015-12-09 22:15:48 -0600

    added styling and sidebar

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module MixedMyth
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # needed to use susy
+    config.compass.require "susy"
   end
 end
